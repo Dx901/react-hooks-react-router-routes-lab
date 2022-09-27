54dcd0026fa9b599a415f747239e6d9b4c2d44b5
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-09-27 21:07:43 +0300

    Completed

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,30 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return(
+    <React.Fragment>
+      <h1>Directors Page</h1>
+
+      {directors.map((director) => (
+        <React.Fragment key={director.name}>
+          <div>
+            {director.name}
+          </div>
+
+          <>
+            {director.movies.map((movies) => (
+              <ul key={movies}>
+                <li>
+                  {movies}
+                </li>
+              </ul>
+            ))}
+          </>
+        </React.Fragment>
+      ))}
+    </React.Fragment>
+  )
+  
 }
 
 export default Directors;
